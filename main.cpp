6c8a1e1dcb1c400152675343baed5b9c94665e16
PrancingPony <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-09-09 09:44:02 -0500

    Add catch

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 
 int main() {
   int number = 0;
-  std::cout << "Enter a number to determine prime status: ";
-  std::cin >> number;
+  bool check_style_stats = true;
+  while (check_style_stats){
+    std::cout << "Enter a number to determine prime status: ";
+    std::cin >> number;
+    if (number % 2 == 0 || number % 5 == 0) {
+      std::cout << "This number is not prime.";
+      return 0;
+    } else {
+      check_style_stats = false;
+    }
+  }
+
   int divide_by = 2;
   bool is_prime = true;
 
   while (divide_by != (number / 2)){
-      if(number % divide_by == 0){
+      if (number % divide_by == 0){
             is_prime = false;
             break;
       }
